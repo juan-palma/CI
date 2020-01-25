5d7dc00f9242c6fc874e3911a731fce2dd5da4af
Juan Palma <juan.palma@me.com>
2020-01-25 17:56:03 -0600

    plantilla de correo para modelo v2

--- application/controllers/Postulate.php
+++ application/controllers/Postulate.php
@@ -354,7 +354,7 @@ class Postulate extends CI_Controller {
 					$fotos = '';
 					if( count($nameFotos) > 0 ){
 						foreach ($nameFotos as $i=>$f) {
-							$fotos .='<div style="width: 25%; display: inline-block; float:left;"><img src="'.$f['file_name'].'" style="width: 100%;"></div>'
+							$fotos .='<div style="width: 25%; display: inline-block; float:left;"><img src="'.$f['file_name'].'" style="width: 100%;"></div>';
 						}
 					}
 					$info['fotos'] = $fotos;
