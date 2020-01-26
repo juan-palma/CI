a4417c246933611055605568862cf4aa055aee80
Juan Palma <juan.palma@me.com>
2020-01-26 11:59:34 -0600

    formulario alianza v5

--- application/controllers/Postulate.php
+++ application/controllers/Postulate.php
@@ -198,6 +198,7 @@ class Postulate extends CI_Controller {
 		$clean = (isset($consulta) && property_exists($consulta, 'contenido_info')) ? str_replace($encontrar, $remplazar, $consulta->contenido_info) : '';
 		$cleanObjecDB = ( is_object(json_decode($clean)) ) ? json_decode($clean) : new stdClass();
 		
+				
 		
 		
 		if($_POST['g-recaptcha-response'] === "" || $_POST['g-recaptcha-response'] === " "){
@@ -231,6 +232,23 @@ class Postulate extends CI_Controller {
 				return false;
 			}
 			
+			$this->basic_modal->clean();
+			$this->basic_modal->tabla = 'postulados';
+			$this->basic_modal->campos = 'id_postulado';
+			$this->basic_modal->condicion = array( "correo" => $_POST['correo'] );
+			
+			$isInicio = $this->basic_modal->genericSelect('sistema');
+			
+			if(count($isInicio) > 0){
+				$this->status = 'personal';
+				$this->errores[] =  "Ya existe un registro con esta cuenta de correo";
+				echo( json_encode(['status' => $this->status, 'valores' => "", 'errores' => $this->errores]) );
+				$this->cleanVar();
+				return false;
+			}
+
+
+			
 			$micarpeta = url_title($_POST['nombre'].'-'.$_POST['apellido'].'-'.date("Y-m-d-His"));
 			if (!file_exists(FCPATH.'assets/public/postulantes/modelo/'.$micarpeta)) {
 			    mkdir(FCPATH.'assets/public/postulantes/modelo/'.$micarpeta, 0777, true);
@@ -239,6 +257,7 @@ class Postulate extends CI_Controller {
 			$this->upload->initialize($config);
 		
 			$rutaImagenes = [];
+			
 			
 			$loadCredencial = false;
 			if ( ! $this->upload->do_upload('credencial') ){
@@ -251,8 +270,7 @@ class Postulate extends CI_Controller {
 				$result = $this->upload->data();
 				$rutaImagenes[] = $result['file_name'];
 			}
-					
-	
+
 			
 			//subir fotos del postulante.
 			$nameFotos = [];
@@ -298,75 +316,57 @@ class Postulate extends CI_Controller {
 				//Consulta - SOMOS
 				$this->basic_modal->clean();
 				$this->basic_modal->tabla = 'postulados';
-				$this->basic_modal->campos = 'id_postulado';
-				$this->basic_modal->condicion = array( "correo" => $_POST['correo'] );
 				
-				$isInicio = $this->basic_modal->genericSelect('sistema');
+				//Consulta INSERT servicios
+				$valores = array( 'correo' => $_POST['correo'], 'seccion' => $_POST['pagina'], 'contenido' => $linea);
+				$insert = $this->basic_modal->genericInsert('sistema', $valores);
+				$this->valores[] = "Se agrego con exito tu registro";
 				
-				//Insertar los valores en la base de datos
-				//Consulta
-				$this->basic_modal->clean();
-				$this->basic_modal->tabla = 'postulados';
+				$this->load->helper('mail');
+				require(VIEWPATH.'admin/customers_parametros.php');
 				
-				if(count($isInicio) > 0){
-					//Consulta UPDATE servicios
-					//$this->basic_modal->condicion = array('id_contenido', $isInicio[0]->id_contenido);
-					//$valores = array('contenido_info' => $linea);
-					//$update = $this->basic_modal->genericUpdate('sistema', $valores);
-					$this->status = 'personal';
-					$this->errores[] =  "Ya existe un registro con esta cuenta de correo";
-				} else{
-					//Consulta INSERT servicios
-					$valores = array( 'correo' => $_POST['correo'], 'seccion' => $_POST['pagina'], 'contenido' => $linea);
-					$insert = $this->basic_modal->genericInsert('sistema', $valores);
-					$this->valores[] = "Se agrego con exito tu registro";
-					
-					$this->load->helper('mail');
-					require(VIEWPATH.'admin/customers_parametros.php');
-					
-					$idaMail_data['destino_mail'][] = $cleanObjecDB->correo;
-					$idaMail_data['origen_mail'] = $cleanObjecDB->correo_form;
-					$idaMail_data['reply_mail'] = $cleanObjecDB->correo;
-					$idaMail_data['username'] = $cleanObjecDB->correo_form;
-					$idaMail_data['password'] = $cleanObjecDB->correo_pass;
-					$idaMail_data['asunto'] = 'Nuevo postulado para Modelo - sitio WEB Circulo de Imagen';
+				$idaMail_data['destino_mail'][] = $cleanObjecDB->correo;
+				$idaMail_data['origen_mail'] = $cleanObjecDB->correo_form;
+				$idaMail_data['reply_mail'] = $cleanObjecDB->correo;
+				$idaMail_data['username'] = $cleanObjecDB->correo_form;
+				$idaMail_data['password'] = $cleanObjecDB->correo_pass;
+				$idaMail_data['asunto'] = 'Nuevo postulado para Modelo - sitio WEB Circulo de Imagen';
 
-					
-					$idaMail_data['destino_mail'][] = "soporte@idalibre.com";
-					$idaMail_data['origen_mail'] = "soporte@idalibre.com";
-					$idaMail_data['reply_mail'] = "soporte@idalibre.com";
-					$idaMail_data['username'] = "soporte@idalibre.com";
-					$idaMail_data['password'] = 'Soporte.libre';
-					
-					
-					$template = FCPATH.'assets/public/template/modeloForm.php';
-					$info = array();
-					$info['nombre'] = $_POST['nombre'];
-					$info['apellido'] = $_POST['apellido'];
-					$info['mail'] = $_POST['correo'];
-					$info['tel'] = $_POST['telefono'];
-					$info['compartir'] = $_POST['compartir'];
-					$info['identificacion'] = base_url('assets/public/postulantes/modelo/').$micarpeta.'/'.$rutaImagenes[0];
-					$info['logo'] = base_url('assets/public/img/logo_ci.svg');
-					$info['empresa'] = 'Circulo Imagen';
-					$info['sitio'] = base_url('postulate/modelo');
-					
-					$fotos = '';
-					if( count($nameFotos) > 0 ){
-						foreach ($nameFotos as $i=>$f) {
-							$fotos .='<div style="width: 25%; display: inline-block; float:left;"><img src="'.base_url('assets/public/postulantes/modelo/').$micarpeta.'/'.$f['file_name'].'" style="width: 100%;"></div>';
-						}
+				
+				$idaMail_data['destino_mail'][] = "soporte@idalibre.com";
+				$idaMail_data['origen_mail'] = "soporte@idalibre.com";
+				$idaMail_data['reply_mail'] = "soporte@idalibre.com";
+				$idaMail_data['username'] = "soporte@idalibre.com";
+				$idaMail_data['password'] = 'Soporte.libre';
+				
+				
+				$template = FCPATH.'assets/public/template/modeloForm.php';
+				$info = array();
+				$info['nombre'] = $_POST['nombre'];
+				$info['apellido'] = $_POST['apellido'];
+				$info['mail'] = $_POST['correo'];
+				$info['tel'] = $_POST['telefono'];
+				$info['compartir'] = $_POST['compartir'];
+				$info['identificacion'] = base_url('assets/public/postulantes/modelo/').$micarpeta.'/'.$rutaImagenes[0];
+				$info['logo'] = base_url('assets/public/img/logo_ci.svg');
+				$info['empresa'] = 'Circulo Imagen';
+				$info['sitio'] = base_url('postulate/modelo');
+				
+				$fotos = '';
+				if( count($nameFotos) > 0 ){
+					foreach ($nameFotos as $i=>$f) {
+						$fotos .='<div style="width: 25%; display: inline-block; float:left;"><img src="'.base_url('assets/public/postulantes/modelo/').$micarpeta.'/'.$f['file_name'].'" style="width: 100%;"></div>';
 					}
-					$info['fotos'] = $fotos;
-					
-					
-					$respMail = ida_sendMail($template, $info, $idaMail_data);
-					if($respMail){
-						$this->valores[] = 'Se envió el correo de manera correcta.';
-					} else{
-						$this->status = 'error';
-						$this->errores[]  = 'Error interno al enviar el correo';
-					}
+				}
+				$info['fotos'] = $fotos;
+				
+				
+				$respMail = ida_sendMail($template, $info, $idaMail_data);
+				if($respMail){
+					$this->valores[] = 'Se envió el correo de manera correcta.';
+				} else{
+					$this->status = 'error';
+					$this->errores[]  = 'Error interno al enviar el correo';
 				}
 				
 				
