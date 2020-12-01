b0d132983ecb236b5587e9b2514cd7a2be2b85a6
Juan Palma <juan.palma@me.com>
2020-11-30 22:39:34 -0600

    Ajuste Politicas Privacidad

--- application/views/public/politicas_privacidad.php
+++ application/views/public/politicas_privacidad.php
@@ -1,14 +1,96 @@
 <div id="aviso" class="mboxG">
 	<div class="mboxC">
 		<div class="contenedor">
-			<h3>AVISO DE CONFIDENCIALIDAD</h3>
-			
-			<p>La información contenida en esta comunicación es enviada únicamente para el uso de la persona o entidad a quien está dirigida y de cualquier otra persona que está autorizada para recibirla. Puede contener información confidencial o legalmente privilegiada. Si usted no es el destinatario, queda en este acto notificado que, en caso de cualquier divulgación, reproducción, distribución o cualquier acción con relación al contenido de esta información, se encuentra estrictamente prohibido y podrá ser ilegal. Si usted ha recibido esta comunicación por error, por favor notifíquenoslo inmediatamente respondiendo a este correo electrónico y después bórrelo de su sistema. inmotion y todas sus entidades no son responsables por la correcta y completa transmisión de la información contenida en esta comunicación, y tampoco por cualquier retraso en su recepción.</p>
-			
-			<hr></hr>
-	
-			<h3>PRIVACY NOTICE</h3>
-			<p>The information contained in this communication is intended solely for the use of the individual or entity to whom it is addressed and any others authorized to receive it. It may contain confidential or legally privileged information. If you are not the intended recipient you are hereby notified that any disclosure, copying, distribution or any action concerning the content of this information is strictly prohibited and may be unlawful. If you have received this communication in error, please notify us immediately by replying to this email and then delete it from your system. inmotion and all its entities are not responsible for the proper and complete transmission of the information contained in this communication nor for any delay in its receipt.</p>
+			<div class="box_politicas">
+				El presente aviso de privacidad, se entrega en cumplimiento a las disposiciones aplicables de la Ley Federal de Protección de Datos Personales en Posesión de Particulares y su respectivo reglamento, con la finalidad de informarle a nuestros usuarios el tratamiento que se dará a la información proporcionada, así como la forma en la cual se puede limitar el uso de los datos recabados. Le informamos que usted puede conocer el contenido de la ley en los portales que el Gobierno Federal, a través de la Secretaría de Gobernación, y la Cámara de Diputados del H. Congreso de la Unión ponen a su disposición:
+				<ul>
+				 	<li> <a href="http://www.ordenjuridico.gob.mx">http://www.ordenjuridico.gob.mx</a></li>
+				 	<li><a href="http://www.diputados.gob.mx/LeyesBiblio/pdf/LFPDPPP.pdf">http://www.diputados.gob.mx/LeyesBiblio/pdf/LFPDPPP.pdf</a></li>
+				 	<li><a href="http://www.diputados.gob.mx/LeyesBiblio/regley/Reg_LFPDPPP.pdf">http://www.diputados.gob.mx/LeyesBiblio/regley/Reg_LFPDPPP.pdf</a></li>
+				</ul>
+				“<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>” pone a su disposición el presente “Aviso de Privacidad” a efecto de garantizar el derecho a la autodeterminación informativa, privacidad, respeto y obligaciones que tenemos con relación al uso adecuado de la información de aquellas personas que nos proporcionan datos personales.
+				
+				De esta manera le manifestamos que “<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>”, a través del portal <a href="http://www.circulodeimagen.com"><b>www.circulodeimagen.com</b></a><b> </b>(en lo sucesivo “<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>”, y se hace responsable del manejo de datos personales, por lo cual reconoce y privilegia la privacidad y confidencialidad de cualquier información que tengan o lleguen a tener en su poder, ya sea de manera temporal o definitiva, incluyendo datos o información personal. Y a fin de dar cumplimiento a lo establecido en la LFPDPPP ha decidido establecer dentro de su esquema organizacional una oficina de Protección de Datos personales, ubicada en Indianápolis 81, 304 Nápoles, Alcaldía Benito Juárez, C.P. 03819, en Ciudad de México y con los siguientes datos de contacto: Teléfono de la oficina de protección de datos: (55) 5161-8684 / (55) 3040-2597.
+				
+				“<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>” reconoce que toda la información personal que se encuentre en su poder ha sido proporcionada de manera libre y voluntaria por sus titulares y/o ha sido adquirida de forma legítima a través de medios legales idóneos bajo el principio de privacidad y confidencialidad de la información, con el objeto de garantizar la seguridad y protección de esta y de sus titulares y al respecto le informa lo siguiente:
+				
+				<b>DATOS PERSONALES QUE SERÁN SOMETIDOS A TRATAMIENTO. </b>
+				
+				“<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>” en la prestación de su servicio tiene la misión de mantener una relación estrecha y activa con sus clientes, empleados, proveedores, prestadores de servicio, y cualquier persona física que tenga que proporcionar datos personales con motivo de una relación jurídica, laboral y/o comercial.
+				
+				En la recolección y tratamiento de los datos personales que usted nos proporciona, cumplimos todos los principios que marca la Ley Federal de Protección de Datos Personales en Posesión de los Particulares y su Reglamento: licitud, calidad, consentimiento, información, finalidad, lealtad, proporcionalidad y responsabilidad. Así como los deberes de seguridad y confidencialidad.
+				
+				Usted puede acceder a nuestro sitio web sin proporcionar datos personales, sin embargo, existen determinadas secciones dentro del mismo en donde puede proporcionar voluntariamente sus datos. Estos pueden ser recabados en distintas situaciones, por ejemplo al llenar el formulario de contacto en nuestro sitio web<b> </b><a href="http://www.circulodeimagen.com"><b>www.circulodeimagen.com</b></a><b>   </b>
+				
+				Los datos personales proporcionados de forma voluntaria y directa; son los que se describen a continuación:
+				
+				<b>DATOS PERSONALES QUE RECABAMOS CUANDO VISITA NUESTRO SITIO DE INTERNET O UTILIZA NUESTROS SERVICIOS EN LÍNEA: </b>
+				<ul>
+				 	<li>Datos de contacto: Nombre, correo electrónico (e-mail), teléfono, mensaje de contacto.</li>
+				 	<li>Datos de identificación: Nombre, domicilio particular, fecha de nacimiento, número de teléfono fijo y/o móvil, fax, correo electrónico, imagen.</li>
+				</ul>
+				Sus datos pueden ser recabados en diferentes situaciones, por ejemplo, cuando decide proporcionarnos su currículum vitae, al enviarnos un correo electrónico, al registrarse para recibir información de interés, así como aquellos que nos proporcione con miras al establecimiento de una relación comercial, por ejemplo, cuando decide contratar nuestros servicios o prestarnos algún servicio. Los datos personales que podemos recabar pertenecen a las siguientes categorías:
+				<ol>
+				 	<li>Datos de identificación: nombre, domicilio particular, fecha de nacimiento, número de teléfono fijo y/o móvil, fax, correo electrónico, imagen, CURP, NSS</li>
+				 	<li>Datos académicos: trayectoria educativa, título, número de cédula profesional, especialidad, certificados.</li>
+				 	<li>Datos laborales: puesto, domicilio, correo electrónico, número de teléfono fijo o móvil y fax.</li>
+				 	<li>Datos de facturación: entidad a la que se hará el cargo, domicilio fiscal, RFC.</li>
+				 	<li>Datos financieros: información de cuentas bancarias, forma de pago.</li>
+				</ol>
+				Garantizando un tratamiento exclusivo dentro del país, directamente a través de “<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>”, quien podrá transferir sus datos personales a sus clientes, subsidiarias, afiliadas o relacionadas. Sus datos personales serán tratados únicamente por el tiempo necesario a fin de cumplir con las finalidades descritas y/o de conformidad con lo que establezcan las disposiciones legales aplicables.
+				
+				“<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>”, es una empresa comprometida con la privacidad de los <b>MENORES E INCAPACES</b>; por ello, no recabamos intencionalmente ni tratamos datos personales de menores e incapaces cuando no se cuenta con el consentimiento expreso de sus padres, tutores o representantes legales. Si Usted es Padre, Tutor o Representante Legal de un menor o incapaz y sabe que nos ha proporcionado datos personales sin su consentimiento, podrá solicitar que los mismos sean cancelados poniéndose en contacto con nuestra Oficina de Protección de Datos ubicada Indianápolis 81, 304 Nápoles, Alcaldía Benito Juárez, C.P. 03819, en Ciudad de México y con los siguientes datos de contacto: Teléfono de la oficina de protección de datos: (55) 5161-8684 / (55) 3040-2597 o bien, a través del sitio web: <a href="http://www.circulodeimagen.com"><b>www.circulodeimagen.com</b></a><b> </b>en el espacio llamado <b>DESCRÍBENOS TU PROYECTO indicando como asunto “protección de datos” y con gusto nos pondremos en contacto con usted. </b>
+				
+				“<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>”<b>, NO RECABA DATOS DE CARÁCTER SENSIBLE,</b> solo recaba aquellos datos que son necesarios para la relación comercial o de subordinación.
+				
+				<b>FINALIDADES DEL TRATAMIENTO Y/O RESGUARDO. </b>
+				
+				“<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>” hace de su conocimiento que los Datos Personales proporcionados voluntariamente por usted, los que se obtengan por referencias personales, en su caso, y aquellos que generemos, durante el uso de los servicios prestados por esta compañía. Podrán ser utilizados para diversas finalidades, dependiendo de las circunstancias particulares en las que se recaban, siempre obedeciendo a la relación que tengamos con usted y al aviso de privacidad específico que, en su caso, se haya puesto a su disposición.
+				
+				<b>Sus datos también podrán ser utilizados para las siguientes finalidades secundarias, siempre que otorgue su consentimiento:</b>
+				<ul>
+				 	<li>Para enviarle ofertas respecto a los servicios que ofrecemos.</li>
+				 	<li>Para enviarle información de su interés.</li>
+				</ul>
+				Así mismo sus datos personales podrían utilizarse para finalidades que sean compatibles con las descritas y puedan considerarse análogas.
+				
+				&nbsp;
+				
+				Como titular de los datos personales recabados y/o tratados por “<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>”usted debe saber que en el momento en que deseé puede manifestar su negativa,  para que sus datos sean tratados de acuerdo a las finalidades secundarias señaladas en líneas anteriores, enviándonos un correo electrónico dirigido a la oficina de protección de datos personales.
+				
+				<b>TRANSFERENCIAS </b>
+				
+				En “<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>” Nos abstendremos de vender, arrendar o alquilar sus datos personales, podremos llegar a compartir sus datos personales con los siguientes terceros:
+				<ul>
+				 	<li>Con las empresas que nos apoyen en las actividades de promoción de nuestros productos, en caso de que haya consentido el tratamiento de sus datos personales con dichas finalidades secundarias;</li>
+				</ul>
+				<ul>
+				 	<li>Con las empresas que nos apoyen en los envíos de mercancía.</li>
+				 	<li>Su información puede ser compartida con Instituciones financieras, bancarias o crediticias, para fines de evaluación del riesgo de fraude de las transacciones.</li>
+				 	<li>Con las autoridades federales y locales, en cumplimiento a las obligaciones contempladas en la legislación aplicable y/o en cumplimiento a requerimientos por parte de estos.</li>
+				</ul>
+				Todo tercero a quién se realice la transferencia señalada, en su caso, manifiesta previamente conocer el presente Aviso de Privacidad y obligarse en sus términos.
+				
+				Sus datos personales no serán transferidos a terceros sin su consentimiento, salvo lo dispuesto por el artículo 37 de “LA LEY”, el cual de manera enunciativa más no limitativa refiere que se podrán transferir los datos entre las sociedades subsidiarias, filiales, afiliadas y controladoras de “<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>”
+				
+				<b>MEDIDAS DE SEGURIDAD Y CONTROL PARA LA PROTECCIÓN DE DATOS PERSONALES</b>
+				
+				“<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>” implementa medidas de seguridad administrativas, técnicas y físicas para proteger sus datos personales, mismas que igualmente exigimos sean cumplidas por los proveedores de servicios que contratamos.
+				
+				<b>DERECHOS CON RESPECTO A SUS DATOS PERSONALES</b>
+				
+				Para su fácil referencia, se describen brevemente los derechos ARCO:
+				<ul>
+				 	<li><b>Acceso.-</b> Que se le informe cuáles de sus datos personales están contenidos en las bases de datos de “<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>” para qué se utilizan dichos datos personales, el origen y las comunicaciones que se hayan realizado con los mismos y, en general, las condiciones del tratamiento de dichos datos.</li>
+				</ul>
+				<ul>
+				 	<li><b>Rectificación.-</b> Que se corrijan o actualicen sus datos personales en caso de que sean inexactos o incompletos.</li>
+				</ul>
+				<ul>
+				 	<li><b>Cancelación.-</b> El derecho que el titular tiene, en todo momento a solicitar a “<b>CÍRCULO DE IMAGEN, S.A. DE C.V.</b>” la cancelación (eliminación) de sus datos personales.</li>
+				 	<li><b>Oposición.-</b> Oponerse por causa legítima al tratamiento de sus datos personales.</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 	<div class="mboxI"></div>
